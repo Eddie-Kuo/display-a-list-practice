b37fd4aa083343a65efcbb01a0f5abb95a00306b
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-04 14:49:41 -0500

    created a separate function for the list

--- app.js
+++ app.js
@@ -75,36 +75,3 @@ for (let videoGame of videoGames) {
 
     
 }
-
-// export const videoGames = [
-//     {
-//         title: 'Super Smash Bros',
-//         system: 'Nintendo Switch',
-//         genre: [
-//             'action',
-//             'strategy',
-//             'fantasy'
-//         ],
-//         multiplayer: 'yes'
-//     },
-//     {
-//         title: 'Pokemon',
-//         system: 'GameBoy',
-//         genre: [
-//             'adventure',
-//             'strategy',
-//             'RPG'
-//         ],
-//         multiplayer: 'no'
-//     },
-//     {
-//         title: 'Mario Party',
-//         system: 'Nintendo Switch',
-//         genre: [
-//             'family',
-//             'strategy',
-//             'fantasy'
-//         ],
-//         multiplayer: 'no'
-//     }
-// ];
--- render-videogame.js
+++ render-videogame.js
@@ -1,23 +1,31 @@
 export function renderVideoGame(videoGameInput) {
     const titleDiv = document.createElement('div');
     const systemTag = document.createElement('p');
-    const genreUl = document.createElement('ul');
-    const genreOneLi = document.createElement('li');
-    const genreTwoLi = document.createElement('li');
-    const genreThreeLi = document.createElement('li');
     const multiplayerTag = document.createElement('p');
+
+    const genreEl = renderGenre(videoGameInput);
 
     titleDiv.textContent = `Game Title: ${videoGameInput.title}`;
     systemTag.textContent = `System: ${videoGameInput.system}`;
-    genreUl.textContent = 'Genre:';
-    genreOneLi.textContent = videoGameInput.genre[0];
-    genreTwoLi.textContent = videoGameInput.genre[1];
-    genreThreeLi.textContent = videoGameInput.genre[2];
     multiplayerTag.textContent = `Multiplayer: ${videoGameInput.multiplayer}`;
 
     titleDiv.classList.add('video-game');
 
-    genreUl.append(genreOneLi, genreTwoLi, genreThreeLi);
-    titleDiv.append(systemTag, genreUl, multiplayerTag);
+    titleDiv.append(systemTag, genreEl, multiplayerTag);
     return titleDiv;
+}
+
+export function renderGenre(genreList) {
+    const genreUl = document.createElement('ul');
+    const genreOneLi = document.createElement('li');
+    const genreTwoLi = document.createElement('li');
+    const genreThreeLi = document.createElement('li');
+
+    genreUl.textContent = 'Genre:';
+    genreOneLi.textContent = genreList.genre[0];
+    genreTwoLi.textContent = genreList.genre[1];
+    genreThreeLi.textContent = genreList.genre[2];
+
+    genreUl.append(genreOneLi, genreTwoLi, genreThreeLi);
+    return genreUl;
 }
